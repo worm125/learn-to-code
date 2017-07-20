09261583ca0ba26a51fa406948b642562af03802
worm125 <anxu37@gmail.com>
2017-07-20 11:54:56 +0530

    Create Find Index.cpp

---
+++ Find Index of target In a sorted array/Find Index.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    int n,target,i;
+    cin >> n;
+    vector<int> arr(n);
+    for(i=0;i<n;i++)
+    cin >> arr[i];
+    cin >> target;
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]<arr[i-1])
+        {
+            cout <<"Array should be sorted in ascending";
+            return 0;
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==target)
+            break;
+        else
+        if(arr[i]>target)
+            break;
+    }
+    cout << i;
+    return 0;
+}
