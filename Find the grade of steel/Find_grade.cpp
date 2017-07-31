82231c7e639f838b1fd225b2f3989097c061882c
Anshuman Mitra <anxu37@gmail.com>
2017-08-01 00:27:21 +0530

    Update Find_grade.cpp

--- Find the grade of steel/Find_grade.cpp
+++ Find the grade of steel/Find_grade.cpp
@@ -5,7 +5,7 @@ int main()
     int hardness,tensile_strength;
     float carbon_content;
     cin >> hardness >> carbon_content >> tensile_strength;
- 
+
     if ((hardness<1 || hardness>10000) || (tensile_strength<1 || tensile_strength>10000))
     {
         cout << "Invalid data either in hardness or tensile strength" << endl;
@@ -16,11 +16,10 @@ int main()
         cout << "Invalid Carbon content" << endl;
         return 0;
     }
-    
     bool condition1 = hardness > 50;
     bool condition2 = carbon_content < .7;
     bool condition3 = tensile_strength > 5600;
-    
+
     if(condition1 || condition2 || condition3)
     {
         if(condition1 && condition2 && condition3)
@@ -34,7 +33,6 @@ int main()
         else 
             grade = 6;
     }
-    
     if(grade>8)
         cout << "The Steel is Very Good with grade as " << g << endl;
     else if(grade<8 && grade>5)
